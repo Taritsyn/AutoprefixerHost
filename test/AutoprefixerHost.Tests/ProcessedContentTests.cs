0a6f621ec76783780137bbb213e98f6aaa2cbeec
Taritsyn <taritsyn@gmail.com>
2020-03-24 21:32:41 +0300

    Added support of the Autoprefixer version 9.7.5

--- test/AutoprefixerHost.Tests/ProcessedContentTests.cs
+++ test/AutoprefixerHost.Tests/ProcessedContentTests.cs
@@ -30,13 +30,11 @@ namespace AutoprefixerHost.Tests
 }";
 			const string targetOutput1 = @".example {
     display: grid;
-    -webkit-transition: all .5s;
     transition: all .5s;
     -webkit-user-select: none;
        -moz-user-select: none;
         -ms-user-select: none;
             user-select: none;
-    background: -webkit-gradient(linear, left top, left bottom, from(white), to(black));
     background: linear-gradient(to bottom, white, black);
 }";
 			const string targetOutput2 = targetOutput1;
