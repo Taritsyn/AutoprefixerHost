52aa02fc0ac4c1c8dfa521c8c0e061547c3aae67
Taritsyn <taritsyn@gmail.com>
2021-09-17 18:42:48 +0300

    Added a benchmarks

---
+++ test/AutoprefixerHost.Benchmarks/CssProcessingBenchmark.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnosers;
+
+namespace AutoprefixerHost.Benchmarks
+{
+	[MemoryDiagnoser]
+	public class CssProcessingBenchmark
+	{
+		private static readonly Dictionary<string, Document> s_documents = new Dictionary<string, Document>
+		{
+			{ "normalize", new Document("normalize.css") },
+			{ "reseter", new Document("reseter.css") },
+			{ "skeleton", new Document("skeleton.css") }
+		};
+
+		public static Dictionary<string, Document> Documents
+		{
+			get { return s_documents; }
+		}
+
+		[ParamsSource(nameof(GetDocumentNames))]
+		public string DocumentName { get; set; }
+
+
+		static CssProcessingBenchmark()
+		{
+			JsEngineSwitcherInitializer.Initialize();
+			Utils.PopulateTestData("Files", s_documents);
+		}
+
+
+		public static IEnumerable<string> GetDocumentNames()
+		{
+			foreach (string key in s_documents.Keys)
+			{
+				yield return key;
+			}
+		}
+
+		[Benchmark]
+		public void Compile()
+		{
+			Document document = Documents[DocumentName];
+			var options = new ProcessingOptions { SourceMap = true };
+
+			using (var autoprefixer = new Autoprefixer(options))
+			{
+				ProcessingResult result = autoprefixer.Process(document.Content, document.RelativePath);
+			}
+		}
+	}
+}
---
+++ test/AutoprefixerHost.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace AutoprefixerHost.Benchmarks
+{
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			BenchmarkRunner.Run<CssProcessingBenchmark>();
+		}
+	}
+}
