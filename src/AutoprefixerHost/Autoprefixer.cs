e674c6d5cd8fcd9095461cc5ec135e3d1b266bed
Taritsyn <taritsyn@gmail.com>
2024-03-03 20:25:56 +0300

    Added support for the Autoprefixer version 10.4.18.0

--- src/AutoprefixerHost/Autoprefixer.cs
+++ src/AutoprefixerHost/Autoprefixer.cs
@@ -87,7 +87,7 @@ namespace AutoprefixerHost
 		/// <summary>
 		/// Gets a version of the Autoprefixer library
 		/// </summary>
-		public string Version => "10.4.17.0";
+		public string Version => "10.4.18.0";
 
 
 		/// <summary>
